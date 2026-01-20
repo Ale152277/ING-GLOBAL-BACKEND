27bbe4af18087219795c02f9997ee5f82bc84057
Ale152277 <gsjoaquin22@gmail.com>
2026-01-20 13:40:20 -0500

    Add files via upload

---
+++ ecommerce/src/main/java/com/ingenieraglobal/ecommerce/IngenieraGlobalECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ingenieraglobal.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngenieraGlobalECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngenieraGlobalECommerceApplication.class, args);
+	}
+
+}
---
+++ ecommerce/src/test/java/com/ingenieraglobal/ecommerce/IngenieraGlobalECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingenieraglobal.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngenieraGlobalECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
