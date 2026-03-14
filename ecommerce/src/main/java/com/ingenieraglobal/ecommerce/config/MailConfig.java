a50831f247779d77e4f04a3748d924f4d5c94aa0
Ale152277 <gsjoaquin22@gmail.com>
2026-03-14 00:58:13 -0500

    .

--- ecommerce/src/main/java/com/ingenieraglobal/ecommerce/config/MailConfig.java
+++ ecommerce/src/main/java/com/ingenieraglobal/ecommerce/config/MailConfig.java
@@ -6,32 +6,32 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
-@Configuration
+//@Configuration (proximamente)
 public class MailConfig {
 
-    @Value("${spring.mail.username}")
-    private String username;
-
-    @Value("${spring.mail.password}")
-    private String password;
-
-    @Bean
-    public JavaMailSender javaMailSender(){
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.resend.com");
-        mailSender.setPort(587);
-        mailSender.setUsername("resend");
-        mailSender.setPassword(password);
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.starttls.required", "true");
-        props.put("mail.smtp.connectiontimeout", "5000");
-        props.put("mail.smtp.timeout", "5000");
-        props.put("mail.smtp.ssl.checkserveridentity", "false");
-        props.put("mail.smtp.ssl.trust", "*");
-
-        return mailSender;
-    }
+//    @Value("${spring.mail.username}")
+//    private String username;
+//
+//    @Value("${spring.mail.password}")
+//    private String password;
+//
+//    @Bean
+//    public JavaMailSender javaMailSender(){
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//        mailSender.setHost("smtp.gmail.com");
+//        mailSender.setPort(587);
+//        mailSender.setUsername(username);
+//        mailSender.setPassword(password);
+//
+//        Properties props = mailSender.getJavaMailProperties();
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.smtp.starttls.required", "true");
+//        props.put("mail.smtp.connectiontimeout", "5000");
+//        props.put("mail.smtp.timeout", "5000");
+//        props.put("mail.smtp.ssl.checkserveridentity", "false");
+//        props.put("mail.smtp.ssl.trust", "*");
+//
+//        return mailSender;
+//    }
 }
--- ecommerce/src/main/java/com/ingenieraglobal/ecommerce/services/Emailservice.java
+++ ecommerce/src/main/java/com/ingenieraglobal/ecommerce/services/Emailservice.java
@@ -4,86 +4,64 @@ import jakarta.mail.internet.MimeMessage; //API que sirve para crear, estructura
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender; //Define metodos (send(mimemessage message)) funciona como un contrato, cualquiera que lo implementa tiene la misma forma de enviar correos
 import org.springframework.mail.javamail.MimeMessageHelper;
+
+import com.resend.Resend;
+import com.resend.services.emails.model.CreateEmailOptions;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Emailservice {
-
-    private final JavaMailSender mailSender;
+    @Value("${resend.api.key}")
+    private String resendApiKey;
 
     @Value("${app.mail.remitente}")
     private String remitente;
-    
 
     @Value("${app.frontend.url}")
-    private String forntendUrl;
-
-    //constructor 
-    public Emailservice(JavaMailSender mailSender){
-        this.mailSender = mailSender;
-    }
-/*
-this.mailSender → se refiere a la propiedad de la clase
-EmailSender → se refiere al parámetro temporal del constructor
-*/
+    private String frontendUrl;
 
 
     public void enviarEmailVerificacion(String destinatario, String nombreCompleto, String token){
-        try{
-            MimeMessage message = mailSender.createMimeMessage(); //Permite crear correos con html, adjuntos, etc..
-            
-            //Clase que facilita la creacion de correos complejos
-            //message ==> Es el message que acabamos de crear
-            //true ==> indica que el mensaje puede tener contenido HTML y adjuntos 
-            //UTF 8 ==> Usa codificacion de caracteres para soportar acentos, ñ, emojis, etc..
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8"); 
-            
-            helper.setFrom(remitente);//configura el remitente del correo
-            helper.setTo(destinatario); //configura el destinatario del correo
-            helper.setSubject("Verifica tu cuenta - Ingeniera Global"); //configura el asunto del correo
+      try {
+        Resend resend = new Resend(resendApiKey);
+        String linkVerificacion = frontendUrl + "/verificar-email?token=" + token;
+        String html = buildVerificacionHtml(nombreCompleto, linkVerificacion);
 
-            
-            //crea un link de verificacion completo
-            //concatena el endpoint con el token unico (" https://miweb.com/verificar-email?token=abc123")
-            String linkVerificacion = forntendUrl + "/verificar-email?token="+ token;
-            
+        CreateEmailOptions params = CreateEmailOptions.builder()
+        .from(remitente)
+        .to(destinatario)
+        .subject("Verifica tu cuenta - D&S Ingenieria Global")
+        .html(html)
+        .build();
 
-            //se llama al metodo buildVerificacion para generar el contenido HTML del correo
-            //pasa el nombre del usuario y el link de verificacion
-            String html = buildVerificacionHtml(nombreCompleto, linkVerificacion);
-            
-
-            //Configura el cuerpo del correo
-            //html ==> contenido
-            //true ==> indica que es HTML, si fuera false sería texto plano
-            helper.setText(html, true);
-
-            //enviar el correo usando el bean JavaMailSender
-            //Spring se encarga de conectarse al servidor SMTP (GMAIL) y enviar el mensaje
-            mailSender.send(message);
-
-        }catch (MessagingException e) {
-            throw new RuntimeException("Error al enviar email de verificación: " + e.getMessage(), e);
-        }
+        resend.emails().send(params);
+        
+      } catch (Exception e) {
+         throw new RuntimeException("Error al enviar email de verificación: " + e.getMessage(), e);
+      }
     }
 
-    public void enviarConsulta(String destinatario, String nombreUsuario, String emailUsuario, String asunto, String mensaje){
-        try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+    public void enviarConsulta(String destinatario, String nombreUsuario, String email, String asunto, String mensaje){
+      try {
+        Resend resend = new Resend(resendApiKey);
 
-            helper.setFrom(remitente);
-            helper.setTo(destinatario);
-            helper.setSubject("[CONSULTA WEB]" + asunto);
-            helper.setText(buildConsultaHtml(nombreUsuario, emailUsuario, asunto, mensaje), true);
+        CreateEmailOptions params = CreateEmailOptions.builder()
+        .from(remitente)
+        .to(destinatario)
+        .subject("[CONSULTA WEB]" + asunto)
+        .html(buildConsultaHtml(nombreUsuario, email, asunto, mensaje))
+        .build();
 
-            mailSender.send(message);
-            
-        } catch (MessagingException e) {
-
-            throw new RuntimeException("Error al enviar consulta"+ e.getMessage(), e);
-        }
+        resend.emails().send(params);
+        
+      } catch (Exception e) {
+        throw new RuntimeException("Error al enviar consulta: " + e.getMessage(), e);
+      }
     }
+
+
+
+
 
     private String buildVerificacionHtml(String nombre, String link) {
         return """
